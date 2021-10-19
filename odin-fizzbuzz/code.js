f88280a2142206ee14628366923568fc99d5e885
Nick Wolff <nick@wolff.tech>
2021-10-19 00:38:39 -0400

    odin: correct solve fizzbuzz

--- odin-fizzbuzz/code.js
+++ odin-fizzbuzz/code.js
@@ -1,15 +1,13 @@
 let answer = parseInt(prompt("Enter a number"));
 
-for (let i = 1; answer <= i; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-    }
-
-    if (i % 3 === 0) {
-        console.log("Fizz");
-    }
-
-    if (i % 5 === 0) {
-        console.log("Buzz");
-    }
+for (let i = 1; i <= answer; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }
