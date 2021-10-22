dc8ad9a368649be0fa5a3a55878575af9ee1cc85
Nick Wolff <nick@wolff.tech>
2021-10-22 12:52:47 -0400

    odin js: solve js-2

--- odin-js-exercises/02_repeatString/repeatString.js
+++ odin-js-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(InputString, NumberOfTimes) {
+    let result = "";
+    if (NumberOfTimes < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < NumberOfTimes; i++) {
+            result += InputString;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
