69957d4b86491ae7345c1d6297704d363bd59c0d
Nick Wolff <nick@wolff.tech>
2021-10-26 01:48:07 -0400

    odin js: solve js-5

--- odin-js-exercises/05_sumAll/sumAll.js
+++ odin-js-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min || !Number.isInteger(max))) {
+        return "ERROR: Please enter integers";
+    }
+    if (min < 0 || max < 0) {
+        return "ERROR: Please enter positive integers";
+    }
+    if (min > max) {
+        return "ERROR: Please enter a valid range";
+    }
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
 };
 
 // Do not edit below this line
