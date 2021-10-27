474bc492aef38eed40fb9543054be1e860e15ab3
Nick Wolff <nick@wolff.tech>
2021-10-27 11:49:21 -0400

    odin js: solve js-9

--- odin-js-exercises/09_palindromes/palindromes.js
+++ odin-js-exercises/09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
-
+// function that cheks if a string is a palindrome
+const palindromes = function (input) {
+    let string = input.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reverse = string.split('').reverse().join('');
+    return string === reverse;
+    }
 // Do not edit below this line
 module.exports = palindromes;
