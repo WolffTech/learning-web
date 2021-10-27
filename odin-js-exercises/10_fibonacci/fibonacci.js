783ec8924a6b5860bfc973d3dcb3addcfd8ca156
Nick Wolff <nick@wolff.tech>
2021-10-27 12:01:47 -0400

    odin js: solve js-10

--- odin-js-exercises/10_fibonacci/fibonacci.js
+++ odin-js-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(arg) {
+	if (arg < 0) {
+		return "OOPS"
+	}
+	if (arg === 0) {
+		return 0
+	}
+	let a = 0;
+	let b = 1;
+	for (let i = 1; i < arg; i++) {
+		const temp = b;
+		b = a + b;
+		a = temp;
+	}
+	return b;
 };
 
 // Do not edit below this line
