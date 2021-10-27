b4107e7ffc24d8d1ce41262b60992cafb70bffc7
Nick Wolff <nick@wolff.tech>
2021-10-27 11:37:33 -0400

    odin js: solve js-8

--- odin-js-exercises/08_calculator/calculator.js
+++ odin-js-exercises/08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  return args.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.length
+  ? args.reduce((acc, curr) => acc * curr)
+  : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(arg) {
+  if (arg === 0) {
+    return 1;
+  }
+  let product = 1;
+  for (let i = 1; i <= arg; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
