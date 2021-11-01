c75c5d2c74be66e0ac5f25fe879fdfbe8086c5fb
Nick Wolff <nick@wolff.tech>
2021-11-01 11:55:46 -0400

    odin js: dom buttons

---
+++ odin-js-exercises/the_dom/button.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector("#btn");
+btn.onclick = () => alert("Hello World 2!");
+
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener("click", () => {
+    alert("Hello World 3!");
+});
+
+function alertFunction() {
+    alert("Hello World 4!");
+}
