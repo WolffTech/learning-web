471908c8e72543af12cd4a1a0cd957e507d1bebe
Nick Wolff <nick@wolff.tech>
2021-11-01 12:10:30 -0400

    odin: delete old files

--- odin-js-exercises/09_palindromes/palindromes.js
+++
@@ -1,8 +0,0 @@
-// function that cheks if a string is a palindrome
-const palindromes = function (input) {
-    let string = input.toLowerCase().replace(/[^a-z0-9]/g, '');
-    let reverse = string.split('').reverse().join('');
-    return string === reverse;
-    }
-// Do not edit below this line
-module.exports = palindromes;
