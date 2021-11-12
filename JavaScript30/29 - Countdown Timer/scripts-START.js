8a72ddcaf3de337e043e2a108978af64461747b6
Nick Wolff <nick@wolff.tech>
2021-11-12 16:49:56 -0500

    js30: complete lesson 29
    created a timer using concepts from previous lessons.

---
+++ JavaScript30/29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,51 @@
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+let countdown;
+
+function timer(seconds) {
+    clearInterval(countdown);
+
+    const now = Date.now(); // current time in milliseconds
+    const then = now + seconds * 1000;
+
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+        if(secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+
+        displayTimeLeft(secondsLeft);
+    }, 1000 );
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+
+    document.title = display;
+    timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    const seconds = end.getSeconds();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer))
