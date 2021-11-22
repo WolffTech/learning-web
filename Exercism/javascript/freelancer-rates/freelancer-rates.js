57777983fdebf771bd73548c92e0b4d6dbdd3752
Nick Wolff <nick@wolff.tech>
2021-11-22 00:48:23 -0500

    exercism-js: solve freelancer rates
    this one was rough with it's last question. took some time to fully
    understand Math.ceil and Math.round, and needed to refresh myself on
    working with percentages.

--- Exercism/javascript/freelancer-rates/freelancer-rates.js
+++ Exercism/javascript/freelancer-rates/freelancer-rates.js
@@ -26,7 +26,7 @@
  * @returns {number} the rate per day
  */
 export function dayRate(ratePerHour) {
-  throw new Error('Implement the dayRate function');
+  return ratePerHour * 8;
 }
 
 /**
@@ -37,7 +37,7 @@ export function dayRate(ratePerHour) {
  * @returns {number} the number of days
  */
 export function daysInBudget(budget, ratePerHour) {
-  throw new Error('Implement the daysInBudget function');
+  return Math.floor(budget / dayRate(ratePerHour));
 }
 
 /**
@@ -49,5 +49,8 @@ export function daysInBudget(budget, ratePerHour) {
  * @returns {number} the rounded up discounted rate
  */
 export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
-  throw new Error('Implement the priceWithMonthlyDiscount function');
+  let numberOfMonths = Math.floor(numDays / 22);
+  let remainingDays = numDays % 22;
+
+  return Math.ceil(dayRate(ratePerHour) * remainingDays + dayRate(ratePerHour) * (numberOfMonths * 22) * (1 - discount));
 }
