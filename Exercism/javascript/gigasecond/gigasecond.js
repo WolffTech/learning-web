846ed5724b5f54970426a49e13e87b4dcaf337b5
Nick Wolff <nick@wolff.tech>
2021-11-24 22:50:08 -0500

    exercism-js: solve gigasecond

---
+++ Exercism/javascript/gigasecond/gigasecond.js
@@ -0,0 +1,9 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const gigasecond = (e) => {
+	return new Date(e.getTime() + Math.pow(10, 12));
+};
+
