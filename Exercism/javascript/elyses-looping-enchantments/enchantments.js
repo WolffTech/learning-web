152436633ee724bd2d8b1b7f2fddc1ccbdf62ac7
Nick Wolff <nick@wolff.tech>
2021-12-14 03:25:11 -0500

    exercism-js: solve elyses looping enchantments

--- Exercism/javascript/elyses-looping-enchantments/enchantments.js
+++ Exercism/javascript/elyses-looping-enchantments/enchantments.js
@@ -9,7 +9,7 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  throw new Error('Implement the cardTypeCheck function');
+    return stack.filter(c => c === card).length;
 }
 
 /**
@@ -20,5 +20,9 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  throw new Error('Implement the determineOddEvenCards function');
+    if (!type) {
+        return stack.filter(c => c % 2 === 1).length;
+    } else {
+        return stack.filter(c => c % 2 === 0).length;
+    }
 }
