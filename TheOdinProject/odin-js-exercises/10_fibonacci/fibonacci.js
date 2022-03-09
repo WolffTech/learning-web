876fdf8dbb4ddb16a4fb7841bc96546a262fa85f
Nick Wolff <nick@wolff.tech>
2022-03-09 15:06:51 -0500

    odin js: review Fibonacci exercise
    - re-did the Fibonacci exercise to review previous lessons.
    - switched to yarn going forward, will be generating yarn.lock files of
      projects going forward.

--- TheOdinProject/odin-js-exercises/10_fibonacci/fibonacci.js
+++ TheOdinProject/odin-js-exercises/10_fibonacci/fibonacci.js
@@ -1,19 +1,15 @@
 const fibonacci = function(arg) {
-	if (arg < 0) {
-		return "OOPS"
-	}
-	if (arg === 0) {
-		return 0
-	}
-	let a = 0;
-	let b = 1;
-	for (let i = 1; i < arg; i++) {
-		const temp = b;
-		b = a + b;
-		a = temp;
-	}
-	return b;
+    if (arg > 0) {
+        let fib = [0, 1];
+        for (let i = 2; i <= arg; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[arg];
+    } else {
+        return 'OOPS';
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
